610fc524e404d1dada41c0729c80e113b0bdb685
dramirezr <diego_ramirez_romero@hotmail.com>
2017-12-18 17:54:02 -0500

    .

--- assets/js/mitrapana.js
+++ assets/js/mitrapana.js
@@ -57,7 +57,9 @@ window.onpopstate = function(event) {
         console.log(hash);
         if ((hash === '') && (page_state==='call')) {
             console.log('history.go(1)');
-          history.go(1); 
+            history.go(1); 
+            alert('Actualmente tiene un servicio de mototaxi asignado. Si realmente desea salir intente nuevamente.');
+            page_state  = 'dashboard';
         }
       }
     });
